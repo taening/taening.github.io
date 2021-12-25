536c6beed64f2e5d137663b5e6147dfdf6a9ea31
SeoPaul <m9472757c@gmail.com>
2021-12-25 14:49:24 +0900

    JavaScript: Section Shifting When Push the Navigation Bar Button

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ window.addEventListener('scroll', ()=>{
     ? navbar.classList.add('navbar--dark') 
     : navbar.classList.remove('navbar--dark');
 })
+
+/* Section Shifting When Push the Navigation Bar Button */
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event)=>{
+    const target = event.target;
+    const link = target.dataset.link;
+    const element = document.querySelector(link);
+    element.scrollIntoView({ behavior: 'smooth' });
+})
