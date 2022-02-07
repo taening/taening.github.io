dfd4542112e36e5a2c51bbfe826abc36d2b96acf
taening <m9472757c@gmail.com>
2022-02-07 23:52:23 +0900

    React, PostCSS: Change Footer Component's className

--- portfolio/src/components/common/footer/footer.jsx
+++ portfolio/src/components/common/footer/footer.jsx
@@ -5,19 +5,17 @@ class Footer extends Component {
   render() {
     return (
       <footer id={styles.footer}>
-        <p className={styles.footer__design}>
-          Designed and Developed by Paul Seo
-        </p>
-        <p className={styles.footer__right}>
+        <p className={styles.author}>Designed and Developed by Paul Seo</p>
+        <p className={styles.copyright}>
           <i className="far fa-copyright"></i>
           2021. Paul Seo. All rights reserved.
         </p>
-        <div className={styles.footer__link}>
+        <div className={styles.siteLink}>
           <a href="https://github.com/taening" target="blank">
             <i className="fab fa-github"></i>
           </a>
           <a href="https://taening.tistory.com" target="blank">
-            <div className={styles.link__logo}></div>
+            <div className={styles.tistoryLogo}></div>
           </a>
         </div>
       </footer>
